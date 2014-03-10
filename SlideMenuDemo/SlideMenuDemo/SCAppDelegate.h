f2b13832a47bad3ae7366d2343adb05f7f1f00c7
shengzhc <schen@ancestry.com>
2014-03-09 22:47:59 -0700

    init

---
+++ SlideMenuDemo/SlideMenuDemo/SCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SCAppDelegate.h
+//  SlideMenuDemo
+//
+//  Created by Shengzhe Chen on 3/9/14.
+//  Copyright (c) 2014 Shengzhe Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SlideMenuDemo/SlideMenuDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SlideMenuDemo
+//
+//  Created by Shengzhe Chen on 3/9/14.
+//  Copyright (c) 2014 Shengzhe Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SCAppDelegate class]));
+    }
+}
